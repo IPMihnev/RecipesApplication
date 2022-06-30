4cafbf1ca79723b3ade102ebe13645430e64360c
IPMihnev <ivan_mihnev@abv.bg>
2022-06-30 21:26:11 +0300

    init

---
+++ src/app/recipes/recipe-list/recipe-list.component.ts
@@ -0,0 +1,24 @@
+import { Component, EventEmitter, OnInit, Output } from '@angular/core';
+import { Recipe } from '../recipe.model';
+
+@Component({
+  selector: 'app-recipe-list',
+  templateUrl: './recipe-list.component.html',
+  styleUrls: ['./recipe-list.component.css']
+})
+export class RecipeListComponent implements OnInit {
+  @Output() recipeWasSelected = new EventEmitter<Recipe>();
+  
+  recipes: Recipe[] = [
+    new Recipe('Tarator', 'Tasty tarator', 'https://live.staticflickr.com/4533/38865861752_3a9ae4115e_b.jpg'),
+  ];
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+  onRecipeSelected(recipe: Recipe) {
+    this.recipeWasSelected.emit(recipe);
+  }
+}
